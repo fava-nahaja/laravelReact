1df808ff1bd22e508745e60e41981de66123999e
Hamid Alizadeh <alizadeh.hamid13@gmail.com>
2018-08-02 11:40:22 +0430

    Update Product seeder

--- database/seeds/ProductsTableSeeder.php
+++ database/seeds/ProductsTableSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use App\Product;
 
 class ProductsTableSeeder extends Seeder
 {
@@ -11,6 +12,15 @@ class ProductsTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $faker = \Faker\Factory::create();
+
+        for ($i = 0; $i < 50; $i++) {
+            Product::create([
+                'title' => $faker->title,
+                'description' => $faker->paragraph,
+                'price' => $faker->randomNumber(2),
+                'availibility' => $faker->boolean(50)                
+            ]);
+        }
     }
 }
