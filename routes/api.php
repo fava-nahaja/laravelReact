f503dcf27b0967236ca95b104d38f2145f63433d
Hamid Alizadeh <alizadeh.hamid13@gmail.com>
2018-08-02 10:45:58 +0430

    Add route for products

--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,7 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('products', function() {
+    return response(['Product 1', 'Product 2', 'Product 3'], 200);
+});
