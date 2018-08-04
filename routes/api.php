289e1207b943b11da39a96dfc1e5bbccf5dd54b2
Hamid Alizadeh <alizadeh.hamid13@gmail.com>
2018-08-04 11:45:48 +0430

    Update product routes

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Http\Request;
+use App\Product;
 
 /*
 |--------------------------------------------------------------------------
@@ -18,25 +19,25 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 });
 
 Route::get('products', function() {
-    return response(['Product 1', 'Product 2', 'Product 3'], 200);
+    return response(Product::all(), 200);
 });
 
 Route::get('products/{product}', function($productId) {
-    return response()->json(['productId' => "{$productId}"], 200);
+    return response(Product::find($productId), 200);
 });
 
-Route::post('products', function() {
-    return  response()->json([
-            'message' => 'Create success'
-        ], 201);
+Route::post('products', function(Request $request) {
+    $resp = Product::create($request->all());
+    return $resp;
 });
  
-Route::put('products/{product}', function() {
-    return  response()->json([
-            'message' => 'Update success'
-        ], 200);
+Route::put('products/{product}', function(Request $request, $productId) {
+    $product = Product::findOrFail($productId);
+    $product->update($request->all());
+    return $product;
 });
  
-Route::delete('products/{product}',function() {
-    return  response()->json(null, 204);
+Route::delete('products/{product}',function($productId) {
+    Product::find($productId)->delete();
+    return 204;
 });
