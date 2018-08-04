febc4f77f81eeceb5f57055207837934eefec22e
Hamid Alizadeh <alizadeh.hamid13@gmail.com>
2018-08-04 12:09:22 +0430

    Update route to call products controller

--- routes/api.php
+++ routes/api.php
@@ -18,26 +18,14 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('products', function() {
-    return response(Product::all(), 200);
-});
 
-Route::get('products/{product}', function($productId) {
-    return response(Product::find($productId), 200);
-});
 
-Route::post('products', function(Request $request) {
-    $resp = Product::create($request->all());
-    return $resp;
-});
+Route::get('products', 'ProductsController@index');
+
+Route::get('products/{product}', 'ProductsController@show');
+
+Route::post('products', 'ProductsController@store');
  
-Route::put('products/{product}', function(Request $request, $productId) {
-    $product = Product::findOrFail($productId);
-    $product->update($request->all());
-    return $product;
-});
+Route::put('products/{product}', 'ProductsController@update');
  
-Route::delete('products/{product}',function($productId) {
-    Product::find($productId)->delete();
-    return 204;
-});
+Route::delete('products/{product}', 'ProductsController@delete');
